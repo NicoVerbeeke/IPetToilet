965352bcab0127a316a95f2504a237e2e8958dd6
Nico Verbeeke <verbeeke.nico@gmail.com>
2016-01-20 09:24:43 -0800

    extra functionaliteit

--- IPetToilet/HondenvoorzieningViewController.swift
+++ IPetToilet/HondenvoorzieningViewController.swift
@@ -1,7 +1,8 @@
 import UIKit
 import MapKit
+import CoreLocation
 
-class HondenvoorzieningViewController: UITableViewController{
+class HondenvoorzieningViewController: UITableViewController, CLLocationManagerDelegate{
     
     var voorziening: Hondenvoorziening!
     
@@ -10,21 +11,78 @@ class HondenvoorzieningViewController: UITableViewController{
     @IBOutlet weak var gemeente: UILabel!
     @IBOutlet weak var plaats: MKMapView!
     @IBOutlet weak var postcode: UILabel!
+    @IBOutlet weak var afstand: UILabel!
+    
+    var center = CLLocationCoordinate2D()
+    //source for getting current location: 
+    // http://stackoverflow.com/questions/25296691/swift-get-users-current-location-coordinates
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
+        
+        
+        //Ask for Authorisation from the user
+        self.locationManager.requestAlwaysAuthorization()
+        
+        // For use in foreground
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
         
         soort.text = voorziening.soort
         plaatsOmschrijving.text = voorziening.plaatsOmschrijving
         gemeente.text = voorziening.gemeente
         postcode.text = voorziening.postcode
         
-        let center = CLLocationCoordinate2D(latitude: voorziening.plaats.latitude, longitude: voorziening.plaats.longitude)
+        center = CLLocationCoordinate2D(latitude: voorziening.plaats.latitude, longitude: voorziening.plaats.longitude)
         let visibleRegion = MKCoordinateRegionMakeWithDistance(center, 1000, 1000)
         plaats.region = visibleRegion
         
         let annotation = MKPointAnnotation()
         annotation.coordinate = center
         plaats.addAnnotation(annotation)
+        
+       /* if CLLocationManager.locationServicesEnabled(){
+            var currentLocation:CLLocationCoordinate2D = self.locationManager.location!.coordinate
+            
+            let currentMapLocation = MKMapPointForCoordinate(currentLocation)
+            
+            let voorzieningLocation = MKMapPointForCoordinate(center)
+            
+            //berekend in km -> /1000
+            let calcAfstand = MKMetersBetweenMapPoints(currentMapLocation, voorzieningLocation) / 1000
+            
+            let afstandString = NSString(format: "%.3f", calcAfstand)
+            
+            afstand.text = (afstandString as String) + " km hiervandaan."
+            
+        }
+        else{
+            afstand.text = "IPetToilet mag uw huidige locatie niet gebruiken."
+        }*/
+
     }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        var currentLocation:CLLocationCoordinate2D = manager.location!.coordinate
+        
+        let currentMapLocation = MKMapPointForCoordinate(currentLocation)
+        
+        let voorzieningLocation = MKMapPointForCoordinate(center)
+        
+        //berekend in km -> /1000
+        let calcAfstand = MKMetersBetweenMapPoints(currentMapLocation, voorzieningLocation) / 1000
+        
+        let afstandString = NSString(format: "%.3f", calcAfstand)
+        
+        afstand.text = (afstandString as String) + " km hiervandaan."
+    }
+    
+    
+
     
 }
