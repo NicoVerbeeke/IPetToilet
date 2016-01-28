90c816af2283f978d88713f5f9b348c9eb6cf59b
Nico Verbeeke <verbeeke.nico@gmail.com>
2016-01-28 08:53:35 -0800

    er wordt nu een pin op de kaart gezet voor iedere hondenvoorziening binnen een straal van 2km

--- IPetToilet/HondenvoorzieningOverViewViewController.swift
+++ IPetToilet/HondenvoorzieningOverViewViewController.swift
@@ -1,26 +1,72 @@
 import UIKit
 import MapKit
 
-class HondenvoorzieningOverViewViewController: UITableViewController{
+class HondenvoorzieningOverViewViewController: UITableViewController, CLLocationManagerDelegate, MKMapViewDelegate{
     var voorzieningen: [Hondenvoorziening]!
     
-    @IBOutlet weak var soort: UILabel!
-    @IBOutlet weak var plaatsOmschrijving: UILabel!
-    @IBOutlet weak var gemeente: UILabel!
     @IBOutlet weak var overzicht: MKMapView!
-    @IBOutlet weak var postcode: UILabel!
+    
+    //source for getting current location:
+    // http://stackoverflow.com/questions/25296691/swift-get-users-current-location-coordinates
+    let locationManager = CLLocationManager()
+    var center = CLLocationCoordinate2D(latitude: 51.05, longitude: 3.7)
+    var centerAnnotation = MKPointAnnotation()
+    var visibleRegion = MKCoordinateRegionMakeWithDistance(CLLocationCoordinate2D(latitude: 51.05, longitude: 3.7), 1000, 1000)
+    
+    var count = 0
     
     override func viewDidLoad() {
+        //Nodig om meerdere annotations weer te geven in de MKMapView
+        self.overzicht.delegate = self
         
+        //Ask for Authorisation from the user
+        self.locationManager.requestAlwaysAuthorization()
+        
+        // For use in foreground
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
+        
+        visibleRegion = MKCoordinateRegionMakeWithDistance(center, 2000, 2000)
+        overzicht.region = visibleRegion
+        
+        
+        centerAnnotation.coordinate = center
+        centerAnnotation.title = "U bevindt zich hier"
+        overzicht.addAnnotation(centerAnnotation)
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let currentLocation:CLLocationCoordinate2D = manager.location!.coordinate
+        
+        let annotationsDrawn = overzicht.annotations
+        overzicht.removeAnnotations(annotationsDrawn)
+        
+        centerAnnotation.coordinate = currentLocation
+        overzicht.addAnnotation(centerAnnotation)
+        
+        visibleRegion = MKCoordinateRegionMakeWithDistance(currentLocation, 2000, 2000)
+        overzicht.region = visibleRegion
 
         
-        /*let center = CLLocationCoordinate2D(latitude: voorziening.plaats.latitude, longitude: voorziening.plaats.longitude)
-        let visibleRegion = MKCoordinateRegionMakeWithDistance(center, 1000, 1000)
-        plaats.region = visibleRegion
+        for index in 1...(voorzieningen.count-1){
+            let voorziening = voorzieningen[index]
+            let location = CLLocationCoordinate2D(latitude: voorziening.plaats.latitude, longitude: voorziening.plaats.longitude)
+            
+            let distance = MKMetersBetweenMapPoints(MKMapPointForCoordinate(center), MKMapPointForCoordinate(location))
+            
+            if(distance <= 2000){
+                let locationAnnotation = MKPointAnnotation()
+                locationAnnotation.coordinate = location
+                overzicht.addAnnotation(locationAnnotation)
+            }
+            
+        }
         
-        let annotation = MKPointAnnotation()
-        annotation.coordinate = center
-        plaats.addAnnotation(annotation)*/
     }
     
 }
